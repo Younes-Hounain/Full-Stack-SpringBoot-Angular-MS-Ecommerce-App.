9beaaab9679ade538f78bf7924eae0a14da1fecc
Younes-Hounain <younesho9@outlook.com>
2023-12-05 01:45:15 +0100

    [CD] : added and fixed customers and orders.

--- ecom-web-app/src/app/app.routes.ts
+++ ecom-web-app/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import {ProductsComponent} from "./products/products.component";
+import {CustomersComponent} from "./customers/customers.component";
+import {OrdersComponent} from "./orders/orders.component";
 
 export const routes: Routes = [
   {
@@ -9,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'customers',
     component: CustomersComponent
+  },
+  {
+    path: 'orders/:customerId',
+    component: OrdersComponent
   }
 ];
--- ecom-web-app/src/app/customers/customers.component.ts
+++ ecom-web-app/src/app/customers/customers.component.ts
@@ -28,6 +28,6 @@ export class CustomersComponent implements OnInit {
   }
 
   getOrders(c: any) {
-
+    this.router.navigate([`/orders/${c.id}`]);
   }
 }
